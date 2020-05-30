288317edaefe82408171a13ec471ff06acc88a13
Brandon Saunders <BrandonNSaunders@gmail.com>
2020-05-30 12:34:29 -0700

    Adjust formatting around social media links.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
-import { Link } from "gatsby"
+import {Link} from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import SocialMediaLinksComponent from '@bit/saundersb.common.social-media-links-component';
@@ -7,42 +7,49 @@ interface HeaderInterface {
     siteTitle: string
 }
 
-const Header = ({ siteTitle }: HeaderInterface) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <SocialMediaLinksComponent/>
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+const Header = ({siteTitle}: HeaderInterface) => (
+    <header
+        style={{
+            background: `steelblue`,
+            marginBottom: `1.45rem`,
+            justifyContent: 'space-between'
+        }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
+        <div style={{
+            display: 'flex',
+            alignContent: 'center',
+            justifyContent: 'flex-end'
+        }}>
+            <SocialMediaLinksComponent/>
+        </div>
+        <div
+            style={{
+                margin: `0 auto`,
+                maxWidth: 960,
+                padding: `1.45rem 1.0875rem`,
+            }}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+            <h1 style={{margin: 0}}>
+                <Link
+                    to="/"
+                    style={{
+                        color: `white`,
+                        textDecoration: `none`,
+                    }}
+                >
+                    {siteTitle}
+                </Link>
+            </h1>
+        </div>
+    </header>
 )
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
+    siteTitle: PropTypes.string,
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+    siteTitle: ``,
 }
 
 export default Header
