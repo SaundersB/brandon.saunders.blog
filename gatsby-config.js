1f80c051a2167625a5d79d78bd19a87cb1b9bacf
Brandon Saunders <BrandonNSaunders@gmail.com>
2020-05-30 11:36:05 -0700

    Adding first blog post.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Brandon Saunders Blog`,
+    description: `A humble blog of my learnings over the years.`,
+    author: `@brandononesaunders`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,8 +13,16 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-catch-links`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -25,6 +33,12 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [], // just in case those previously mentioned remark plugins sound cool :)
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
---
+++ src/pages/05-30-2020-new-blog/index.md
@@ -0,0 +1,7 @@
+---
+path: "/new-blog"
+date: 2020-05-30T17:12:33.962Z
+title: "My first blog post"
+---
+
+This is my first blog post
